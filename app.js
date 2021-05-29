f4f528a19b27d140c423f8ee73d0055bf0f36eaf
wlight96 <wlight96@naver.com>
2021-05-29 17:30:00 +0900

    server update

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const cors = require("cors");
+const bodyParser = require("body-parser");
 app.use(cors());
 const io = require("socket.io")(server, {
 	cors: { origin: "*", methods: ["GET", "POST"], credentials: true },
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3001;
 
@@ -27,14 +31,17 @@ var wordArr = [
 	"다이어리",
 	"가방",
 ];
-// 라이어 
+// 라이어
 var liar;
+
+let votecnt = 0;
+let voteState = [];
+let voteToLiar = 0;
 
 io.on("connection", (socket) => {
 	console.log("a user connected");
 	// PORT에 socket id 알려주기
 	//io.to(socket.id).emit('my socket id',{socketID, socket.id});
-
 	// 플레이어 들어올 때 마다 배열에 플레이어 이름 추가 되어 전송?
 	socket.on("enter", (data) => {
 		console.log(socket.id, ":", data.player, "입장");
@@ -43,21 +50,29 @@ io.on("connection", (socket) => {
 			state: "enter",
 			player: UserID,
 		});
-    if (UserID.length == 4){
-      var randomNumber = Math.floor(Math.random() * 10);
-      // 라이어 기록하기.
-      liar = UserID[randomNumber%4];
-      console.log("game start. liar is ",liar)
-      // game set response
-      io.emit("gameset",{
-        state : "gameset",
-        player: UserID,
-        liar : liar,
-        word: wordArr[randomNumber],   
-      });
-    }
+		// uer4명일 때, 게임 시작 
+		if (UserID.length === 4) {
+			var randomNumber = Math.floor(Math.random() * 10);
+			// 라이어 기록하기.
+			liar = UserID[randomNumber % 4];
+			console.log("game start. liar is ", liar);
+			// game set response
+			io.emit("gameset", {
+				state: "gameset",
+				player: UserID,
+				liar: liar,
+				word: wordArr[randomNumber],
+			});
+			setTimeout(() => {
+				console.log("result liar : ",liar);
+				io.emit("result", {
+					state: "result",
+					liar: liar,
+					picked: voteState,
+				});
+			}, 300000);
+		}
 	});
-
 	// 채팅 관련 송신 및 수신
 	socket.on("chat", (data) => {
 		console.log(data.player, ": ", data.text);
@@ -67,12 +82,34 @@ io.on("connection", (socket) => {
 			text: data.text,
 		});
 	});
-
-	// vote data
-	socket.on("vote", (data) => {
+	// vote on response
+	app.post("/",(req,res) =>{
+		votecnt +=1;
+		try {
+			if (req.body.vote === liar) {
+				voteToLiar++;
+				console.log(voteToLiar);
+			}
+			voteState.push(req.body);
+			console.log(voteState);
+		} catch (e) {
+			console.log(e);
+		}
+		// 그냥 형식적으로 보내주는 리스폰스
+		// 전부 소켓으로 하면 편하긴 한데 패킷 종류를 여러개로 해야되니까 어거지로 넣음..
+		res.send({ result: liar });
+		if (votecnt === 3)
+			io.emit("end", {
+				state: "end",
+				liar: liar,
+				picked: voteState,
+			});	
+	});
+	// vote data // 투표는 2번 하기 근데 이건 언제 하는데?
+	/*socket.on("vote", (data) => {
 		console.log(data.name, " vote to : ", data.vote);
 		VoteID.push({ name: data.name, vote: data.vote });
-		if (VoteID.length == 4) {
+		if (VoteID.length === 4) {
 			io.emit("result", {
 				state: "result",
 				liar: UserID[1],
@@ -82,12 +119,13 @@ io.on("connection", (socket) => {
 			});
 		}
 	});
-
+	*/
 	socket.on("disconnect", () => {
 		console.log("user disconnected");
 	});
 });
 
+
 server.listen(port, () => {
-	console.log("Listening on port ${port}");
+	console.log(`Listening on port ${port}`);
 });
